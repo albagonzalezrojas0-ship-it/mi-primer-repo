805a8ff88cc5854d4b414957000185d08bbb928a
albagonzalezrojas0-ship-it <albagonzalezrojas0@gmail.com>
2026-06-02 08:23:08 -0600

    Add ratio calculation with zero division check

---
+++ ratio.py
@@ -0,0 +1,7 @@
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+
+if b != 0:
+    print("La razón es:", a / b)
+else:
+    print("No se puede dividir entre cero.")
